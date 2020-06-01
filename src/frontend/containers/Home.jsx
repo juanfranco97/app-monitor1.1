8dbccacd31b1014bb3ee1e4c69f7c0c041b609ff
juan franco <juanfranco1997mi@gmail.com>
2020-05-31 19:34:05 -0500

    monitor1.1

---
+++ src/frontend/containers/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import '../assets/styles/App.scss';
+
+const Home = ({ myList, trends, originals }) => {
+  return (
+    <>
+      <Header />
+      <Search isHome />
+      {myList.length > 0 && (
+        <Categories title='Mis monitoreos'>
+          <Carousel>
+            {myList.map(item => (
+              <CarouselItem
+                key={item.id}
+                {...item}
+                isList
+              />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title='Boquillas GOB'>
+        <Carousel>
+          {trends.map(item => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Categories title='Otras boquillas OIP'>
+        <Carousel>
+          {originals.map(item => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
---
+++ src/frontend/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <h1>No Encontrado</h1>
+    <h2>Esta funcion esta en desarrollo regresa al home</h2>
+  </>
+);
+
+export default NotFound;
